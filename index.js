f2e9e45483d0f31da5319d09a0f3b3636727babb
S-Khan786 <osahilkhan654@gmail.com>
2025-02-13 20:51:27 +0530

    Added route path in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 
 const db = require('./config/mongoose.js');
@@ -14,6 +15,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
